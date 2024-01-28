f2db4915a79d83f59342568be8c24929175a4599
unsalgokdag <unsal.gokdag@gmail.com>
2024-01-28 02:52:15 +0100

    tidy up

--- einsum.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from einsum_cython import generalized_einsum
-
-A = np.array([[1, 2], [3, 4]], dtype=np.float32)
-B = np.array([[5, 6], [7, 8]], dtype=np.float32)
-
-result = generalized_einsum(A, B, b'ij,ij->ijkl')
-print(result)
